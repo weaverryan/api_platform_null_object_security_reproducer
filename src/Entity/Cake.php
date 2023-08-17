2d16dcc2eabeb5590ec2d0a9a31a693c3da57bd9
Ryan Weaver <ryan@symfonycasts.com>
2023-08-17 11:33:56 -0400

    Adding Cake entity

---
+++ src/Entity/Cake.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CakeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CakeRepository::class)]
+#[ApiResource]
+class Cake
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $flavor = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFlavor(): ?string
+    {
+        return $this->flavor;
+    }
+
+    public function setFlavor(string $flavor): static
+    {
+        $this->flavor = $flavor;
+
+        return $this;
+    }
+}
